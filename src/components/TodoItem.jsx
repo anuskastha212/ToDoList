e6bdad271e29179d51a079db33db1ca95d090bff
Anuska <anuskastha212@gmail.com>
2026-01-14 10:36:25 +0545

    Updated UI

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -11,7 +11,6 @@ function TodoItem({ todo }) {
     setIsTodoEditable(false)
   }
   const toggleCompleted = () => {
-    //console.log(todo.id);
     toggleComplete(todo.id)
   }
 
@@ -36,7 +35,6 @@ function TodoItem({ todo }) {
               onChange={(e) => setTodoMsg(e.target.value)}
               readOnly={!isTodoEditable}
           />
-          {/* Edit, Save Button */}
           <button
               className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
               onClick={() => {
@@ -50,7 +48,6 @@ function TodoItem({ todo }) {
           >
               {isTodoEditable ? "📁" : "✏️"}
           </button>
-          {/* Delete Todo Button */}
           <button
               className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
               onClick={() => deleteTodo(todo.id)}
